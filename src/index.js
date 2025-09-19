a1ffa1368d46c4fd734de69d5fa096a52d5b11ce
Anil Kumar <anilchowdary538@gmail.com>
2025-09-19 16:23:56 +0100

    Add default React entry files (src/index.js and public/index.html) for Vercel deployment readiness.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Dashboard from "./components/Dashboard";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Dashboard />
-  </React.StrictMode>
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
